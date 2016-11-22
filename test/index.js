9782fda3f2bc1f69d463e46e933467307b5046aa
Den Williams <dennis.williams@loke.com.au>
2016-11-23 00:18:52 +1100

    fixes

--- test/index.js
+++ test/index.js
@@ -3,15 +3,17 @@ const inversify = require('inversify');
 const inversifyLite = require('..');
 
 test('something', t => {
-  const container = inversifyLite.create({ inversify })
-  .register('one', one)
-  .transient('two', two);
+  const container = inversifyLite.create({ inversify, require })
+  .transient('one', one)
+  .transient('./fixtures/some-service')
+  .singleton('two', two);
 
-  container.get('one');
-  container.get('one');
+  // container.get('one');
+  // container.get('one');
+  container.run(one => {});
 });
 
-function one(two) {
+function one(two, someService) {
   console.log('one');
 }
 
